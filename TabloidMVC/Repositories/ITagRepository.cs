171f18c2d28cce43cad2de5ee3871be57bae4017
Jon Newton <jonn@hey.com>
2021-05-19 16:20:21 -0400

    Created Tag model, repository, and interface. Added GetAllTags method to TagRepository.cs

---
+++ TabloidMVC/Repositories/ITagRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+				interface ITagRepository
+				{
+								public List<Tag> GetAllTags();
+				}
+}
---
+++ TabloidMVC/Repositories/TagRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+using TabloidMVC.Utils;
+
+namespace TabloidMVC.Repositories
+{
+				public class TagRepository : BaseRepository, ITagRepository
+				{
+								public TagRepository(IConfiguration config) : base (config) { }
+								
+								public List<Tag> GetAllTags()
+								{
+												using (var conn = Connection)
+												{
+																conn.Open();
+																using (var cmd = conn.CreateCommand())
+																{
+																				cmd.CommandText = @"
+																								SELECT		Id, 
+																																[Name]
+																								FROM
+																																Tag
+																				";
+																				SqlDataReader reader = cmd.ExecuteReader();
+
+																				List<Tag> tags = new List<Tag>();
+
+																				while (reader.Read())
+																				{
+																								tags.Add(NewTagFromReader(reader));
+																				}
+
+																				reader.Close();
+
+																				return tags;
+																}
+												}
+								}
+
+								private Tag NewTagFromReader(SqlDataReader reader)
+								{
+												return new Tag()
+												{
+																Id = reader.GetInt32(reader.GetOrdinal("Id")),
+																Name = reader.GetString(reader.GetOrdinal("Name"))
+												};
+								}
+				}
+}
