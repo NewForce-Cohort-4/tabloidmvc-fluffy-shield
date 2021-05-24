ece57a0f18637139f708971d4afa26f03b8839fb
Sophia <soapsstone@gmail.com>
2021-05-24 09:25:13 -0400

    view all tags

---
+++ TabloidMVC/Controllers/TagController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    [Authorize]
+    public class TagController : Controller
+    {
+
+        private readonly ITagRepository
+           _tagRepositroy;
+
+        public TagController(ITagRepository tagRepository)
+        {
+            _tagRepositroy = tagRepository;
+        }
+
+        // GET: TagController 
+        public ActionResult Index()
+        {
+            var tags = _tagRepositroy.GetAllTags();
+            return View(tags);
+        }
+
+        // GET: TagController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
